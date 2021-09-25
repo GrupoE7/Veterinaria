840e1176b76d3d042c47d7e0bfb17cc01e48b9f3
zuany <zuany1706@gmail.com>
2021-09-25 16:34:27 -0500

    bootrtrap y conexion bd

---
+++ src/views/Homepage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hpage">
+    
+    <Homepage msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import HomePage from '../components/HomePage.vue'
+
+export default {
+  name: 'Homepage',
+  props: {
+    msg: String
+  }
+}
+
+</script>

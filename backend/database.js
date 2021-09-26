14d6be84f022d58c7576a7981afe39d3fb104d90
acalderonmor <86635497+acalderonmor@users.noreply.github.com>
2021-09-26 04:11:02 -0500

    Se creo el portafolio desde DB

--- backend/database.js
+++ backend/database.js
@@ -2,8 +2,14 @@
 //   db: "mongodb://localhost:27017/vuecrudmevn",
 // };
 
-module.exports = {
-  db:
-  "mongodb+srv://GrupoE788:1024502097@cluster0.sotsw.mongodb.net/Cluster0?retryWrites=true&w=majority" 
+   module.exports = {
+   db:
+   "mongodb+srv://GrupoE788:1024502097@cluster0.sotsw.mongodb.net/Cluster0?retryWrites=true&w=majority" 
  };
+ 
+//  module.exports = {
+//   db:
+//   "mongodb+srv://root:root@cluster0.xawwb.mongodb.net/Cluster0?retryWrites=true&w=majority" 
+//  };
+
  
